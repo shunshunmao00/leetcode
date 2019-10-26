f9a2b743d78ea544437f4983b3fc878107990123
xu <xuzlfight@qq.com>
2019-10-26 09:41:27 +0800

    26. 删除排序数组中的重复项

---
+++ leet_002.py
@@ -0,0 +1,21 @@
+# Definition for singly-linked list.
+class ListNode:
+    def __init__(self, x):
+        self.val = x
+        self.next = None
+
+class Solution:
+    def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
+        temp = 0
+        re_head = ListNode()
+        re_head.val = 0
+        re_node = re_head
+        if l1 != None and l2 != None:
+
+    def add(self, ):
+
+        pass
+
+
+if __name__ == "__main__":
+    pass
